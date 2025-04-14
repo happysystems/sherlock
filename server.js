e67a64a536a5ac96f35518764e8072740489bcde
happysystems <lolhahaxdimhappy@outlook.com>
2025-04-14 09:18:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3000; // Render uses process.env.PORT
 
 // Serve static files (optional, if you have a frontend folder)
-app.use(express.static(path.join(__dirname, "frontend")));
+//app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "home.html"));
+  res.sendFile("home.html"));
 });
 
 app.listen(PORT, () => {
